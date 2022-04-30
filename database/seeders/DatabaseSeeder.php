4364833c40ff0f7da51e0a0478c5033d9596270d
trascent <luchopodex@hotmail.com>
2022-04-30 13:00:35 -0500

    Creación de seeders para insertar información a la base de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        $this->call([
+            // Facturas Base
+            BillSeeder::class,
+            // Productos Base
+            ProductSeeder::class,
+            // Productos comprados Base
+            PurchasedProductSeeder::class,
+        ]);
     }
 }
